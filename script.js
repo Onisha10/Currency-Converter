6c45e9c37d0266755c0fe97236805404a05f70f4
Onisha10 <151004223+Onisha10@users.noreply.github.com>
2023-11-16 12:16:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function convertCurrency() {
+    const amount = document.getElementById('amount').value;
+    const fromCurrency = document.getElementById('fromCurrency').value;
+    const toCurrency = document.getElementById('toCurrency').value;
+
+    if (amount === '' || isNaN(amount)) {
+        alert('Please enter a valid amount');
+        return;
+    }
+
+    const apiUrl = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const exchangeRate = data.rates[toCurrency];
+            const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+            document.getElementById('result').innerHTML = `
+                ${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}
+            `;
+        })
+        .catch(error => {
+            console.error('Error fetching exchange rates:', error);
+        });
+}
